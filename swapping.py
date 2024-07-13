51787ea95cc31c2e1c7e8943e3d1dfd30133b362
vaishnavisarge <vaishnavisarge@gmail.com>
2024-07-13 22:10:33 +0530

    Create swapping.py

---
+++ swapping.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+print("Before Swapping numbers are:",n1,n2)
+temp=n1
+n1=n2
+n2=temp
+print("After Swapping numbers are:",n1,n2)
