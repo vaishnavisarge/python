fb7298595bac6fd36134bdeda8b08139e439bf45
vaishnavisarge <vaishnavisarge@gmail.com>
2024-07-19 20:56:42 +0530

    Create reverse_vowels_of_string.py

---
+++ reverse_vowels_of_string.py
@@ -0,0 +1,17 @@
+def reverseVowels(s):
+    s=list(s)
+    vowels=('s','e','i','o','u','s','I','O','U','E')
+    i=0
+    j=len(s)-1
+    while i<j:
+        if s[i] in vowels and s[j] in vowels:
+            s[i],s[j]= s[j],s[i]
+            i=i+1
+            j=j-1
+        if s[i] not in vowels:
+            i+=1
+        if s[j] not in vowels:
+            j-=1
+    return "".join(s)
+print(reverseVowels('hello'))
+
